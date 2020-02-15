7a9415a3d26a72c454d4636fd668f378f6941895
natsu08-n <merrysheep18@gmail.com>
2020-02-15 15:56:18 +0900

    add new file

---
+++ head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+   <head>
+    <meta charset="utf-8">
+    <title>DramaLog | <?php echo $siteTitle; ?> </title>
+    <link href="https://fonts.googleapis.com/css?family=Sawarabi+Mincho|Noto+Serif|Pacifico&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    
+   
+</head>
